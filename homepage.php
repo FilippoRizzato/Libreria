847c695ec63bf643e9fe9262ea29f49be97836b5
FilippoRizzato <94640711+FilippoRizzato@users.noreply.github.com>
2025-01-26 15:11:55 +0100

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -1,24 +1,17 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>Gestione Libreria</title>
+
 </head>
 <body>
-    <h1>Libreria</h1>
-    <div class="topnav">
-        <a href="create.php"> Crea un Libro</a>
-        <a href="read.php"> Leggi un Libro</a>
-        <a href="update.php"> Aggiorna il prezzo di un Libro</a>
-        <a href="delete.php"> Rimuovi un Libro</a>
-    </div>
-
-    <div class="footer">
-        <p>FilippoRizzato&copy</p>
-    </div>
+<h1>Benvenuto nella Gestione Libreria</h1>
+<img src="immagine_di_libreria.jpg" alt="Libreria" style="width:50%;height:auto;">
+<nav>
+    <a href="create.php">Inserisci Libro</a> |
+    <a href="read.php">Visualizza Libri</a> |
+    <a href="update.php">Aggiorna Prezzo</a> |
+    <a href="delete.php">Rimuovi Libro</a>
+</nav>
 </body>
 </html>
